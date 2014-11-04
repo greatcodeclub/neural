d0b4cb2145cf5161509e8fcc99212303b847e8c3
macournoyer <macournoyer@gmail.com>
2014-11-03 22:05:47 -0500

    Renaming

--- demo.js
+++ demo.js
@@ -1,1 +1,26 @@
-var Neuron = require("./lib/neuron").Neuron
+var NetworkNetwork = require('./lib/neural').Network
+var network = new NetworkNetwork()
+
+// Layers
+network.addLayer(5, 2) // Hidden layer: 5 neurons, 2 inputs each (# inputs = # inputs to process)
+network.addLayer(4)    // Hidden layer: 4 neurons
+network.addLayer(1)    // Output layer: 1 neuron, outputting the result
+
+network.train([
+  // Training examples
+  // inputs   outputs
+  [  [0, 0],  [0]  ],
+  [  [0, 1],  [1]  ],
+  [  [1, 0],  [1]  ],
+  [  [1, 1],  [0]  ]
+])
+
+// Querying the network
+var output = network.process([1, 0]) // => [1]
+console.log(output, Math.round(output))
+
+var output = network.process([0, 0]) // => [0]
+console.log(output, Math.round(output))
+
+var output = network.process([1, 1]) // => [0]
+console.log(output, Math.round(output))
