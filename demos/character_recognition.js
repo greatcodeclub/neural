26c612c0777eff0aad09df1b567c19be7c565cd3
macournoyer <macournoyer@gmail.com>
2014-11-06 08:45:47 -0500

    Add outputs as comments in demo.

--- demos/character_recognition.js
+++ demos/character_recognition.js
@@ -9,7 +9,7 @@ var outputs = network.process([
 // outputs === [~1, ~0]
 
 // Convert the output to binary (base 2) and then to decimal (base 10).
-var binary  = outputs.map(function(v) { return Math.round(v) }).join("")
-var decimal = parseInt(binary, 2)
+var binary  = outputs.map(function(v) { return Math.round(v) }).join("") // => "10"
+var decimal = parseInt(binary, 2)  // => 2
 
 console.log("Recognized", decimal, outputs)
